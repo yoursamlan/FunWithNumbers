ce12df19a68c2f31314bb1162cc44d25d62b0662
Amlan Saha Kundu <skamlan99@gmail.com>
2020-05-10 05:44:22 +0530

    Create Simplest Factor.py

---
+++ Simplest Factor.py
@@ -0,0 +1,8 @@
+num = int(input("Enter number: "))
+def factors(n):
+    flist = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            flist.append(i)
+    return flist
+    print(factors(num))
